0d0eadeb5c5e52558425fcaeaec2fecee3d5675b
Mendenhk <kris_mendenhall@yahoo.com>
2025-11-02 15:01:29 -0800

    started debugging program week2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,30 +1,21 @@
-// const input = document.querySelector('#favchap');
-// const button = document.querySelector('button');
-// const list = document.querySelector('#list');
-// const li = document.createElement('li');
-// const deleteButton = document.createElement('button');
-
-// li.textContent = input.value;
-// deleteButton.textContent = '❌';
-// li.append(deleteButton);
-// list.append(li);
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', function () {
     if (input.value.trim() !== '') {
-        const input = document.querySelector('#favchap');
-        const button = document.querySelector('button');
-        const list = document.querySelector('#list');
         const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-
         li.textContent = input.value;
+        const deleteButton = document.createElement('button');
         deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+        })
         li.append(deleteButton);
         list.append(li);
     }
-
-    deleteButton.addEventListener('click', function () {
-        list.removeChild(li);
-        input.focus();
-    })
+    //removes values from the input box
+    input.value = '';
+    //places curser back in box
+    input.focus();
 })
