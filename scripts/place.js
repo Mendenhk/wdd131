c4ece76894d42a856310800bcc08fc5a9f291b36
Mendenhk <kris_mendenhall@yahoo.com>
2025-11-11 18:18:16 -0800

    finished homework

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,23 @@
+//calculating and entering wind chill factor
+const temperature = 31;
+const windSpeed = 5;
+let windChill;
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = calculateWindChill(temperature, windSpeed);
+} else {
+    windChill = 0;
+}
+
+document.querySelector('.wind-chill').innerHTML = `${windChill}°C`
+
+function calculateWindChill(temperature, windSpeed) {
+    let windChill;
+    windChill = 13.12 + 0.6215 * temperature - 11.37 * windSpeed ** 0.16 + 0.3965 * temperature * windSpeed ** 0.16
+    return windChill;
+}
+
+
+//footer javaScript
 const today = new Date();
 const year = document.querySelector("#currentYear");
 year.innerHTML = `${today.getFullYear()}`
